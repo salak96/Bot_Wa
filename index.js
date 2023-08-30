74799025f133d7d597f024e3cdb7a36eb045a431
lambang <sasangkakurniawan.com>
2023-08-30 14:08:09 +0700

    perubahan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
 const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const owner = ["62859106918620"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
